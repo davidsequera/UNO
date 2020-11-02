b48fd0b0115ddac1b2ab9b61d41f4b487e61c2fa
David Sequera <davidsequerads@gmail.com>
2020-11-02 17:23:52 -0500

    Clean main

--- UNO/UNO.cpp
+++ UNO/UNO.cpp
@@ -1,4 +1,4 @@
-// UNO.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// UNO.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
