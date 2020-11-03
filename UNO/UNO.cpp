7fdc79fae2f1ccf1b4ec1d50fe6450b3a2f66e18
David Sequera <davidsequerads@gmail.com>
2020-11-02 22:20:46 -0500

    Mazo Inicial y Jugadores Falsos

--- UNO/UNO.cpp
+++ UNO/UNO.cpp
@@ -1,20 +1,120 @@
-// UNO.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <math.h>
+#include <time.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+const int CARDS = 108;
+
+
+
+class Card {
+public:
+    string type;
+    string color;
+    int value;
+};
+
+class Player {
+public:
+    string nickname;
+    int cantCartas;
+    int score;
+    Card deck[10];
+    void play() {
+        //pone una carta
+    }
+};
+
+class Table {
+public:
+    Card lastCard;
+    Card picking[CARDS];
+    void createCards() {
+        srand(time(NULL));
+        create();
+        mix();
+    };
+    void showDeck() {
+        for (int i = 0; i < CARDS; i++) {
+            cout << picking[i].color << " " << picking[i].value << " " << picking[i].type << endl;
+        }
+    }
+private:
+    void deleteCard() {
+        ;
+    }
+    void create() {
+        for (int i = 0; i < CARDS; i++) {
+            if (i < 25) { picking[i].color = "Blue"; }
+            if (i < 50 && i >= 25) { picking[i].color = "Red"; }
+            if (i < 75 && i >= 50) { picking[i].color = "Yellow"; }
+            if (i < 100 && i >= 75) { picking[i].color = "Green"; }
+            if (i < 108 && i >= 100) { picking[i].color = "Black"; }
+        }// Para los colores
+        int value = 1;
+        for (int i = 0; i < CARDS; i++) {
+
+            if (i < (CARDS - 8)) {
+            //antes de las cartas negras
+
+                picking[i].value = value <= 12 ? value : (value - 13);
+                // para valores mayores a 9 se trata de cartas especiales
+                // Toma 2 == 10; Cambio de sentido == 11;Pierde turno == 12; 
+
+                picking[i].type = picking[i].value < 10 ? "Normal" : "Special";
+
+                value = value == 25 ? 0 : value; // Sistema para creae solo un 0
+                value++;
+            }
+            if (i >= (CARDS - 8)) {
+                picking[i].type = "Special";
+                picking[i].value = i < (CARDS - 4) ? 13 : 14;
+                //Cambio de color == 13 ; Toma 4 == 14
+            }
+
+        }// Para los valores y tipo
+    }
+    void mix(){
+        for (int i = 0; i < CARDS; i++) {
+            int random = rand() % CARDS;
+            Card temp = picking[i];
+            picking[i] = picking[random];
+            picking[random] = temp;
+        }
+    }
+};
+
+
+void mockingPlayers(Player v[], Table);
+
+int main() {
+    Table theTable;
+    Player players[4];
+    theTable.createCards();
+    theTable.showDeck();
+    mockingPlayers(players, theTable);
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void mockingPlayers(Player players[], Table table) {
+    players[0].nickname = "Martin";
+    players[1].nickname = "Jhoseph";
+    players[2].nickname = "Jaider";
+    players[3].nickname = "David";
+    int ca = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j= 0; j < 7; j++) {
+            players[i].deck[j] = table.picking[ca];
+            ca++;
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        cout << players[i].nickname<< ": " <<  endl;
+        for (int j = 0; j < 7; j++) {
+            cout << " " << players[i].deck[j].color << " " << players[i].deck[j].value << " " << players[i].deck[j].type << endl;
+        }
+    }
+}
+
